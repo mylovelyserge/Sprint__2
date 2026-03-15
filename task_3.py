c5cb84d7429040315abc5221448e1270870110f6
mylovelyserge <mylovelyserge@yandex.ru>
2026-03-15 16:47:48 +0300

    Правка третьей задачи

--- task_3.py
+++ task_3.py
@@ -1,42 +1,36 @@
 class PointsForPlace:
-    def __init__(self):
-        self.points = 0
 
-    def get_points_for_place(self, place):
+    @staticmethod
+    def get_points_for_place(place):
+        points = 0
+
         if place > 100:
-            return 'Баллы начисляются только первым 100 участникам'
+            print('Баллы начисляются только первым 100 участникам')
         elif place < 1:
-            return 'Спортсмен не может занять нулевое или отрицательное место'
+            print('Спортсмен не может занять нулевое или отрицательное место')
         else:
-            self.points = 101 - place
-            return self.points
+            points = 101 - place
+
+        return points
 
 class PointsForMeters:
-    def __init__(self):
-        self.points = 0
 
-    def get_points_for_meters(self, meters):
+    @staticmethod
+    def get_points_for_meters(meters):
+        points = 0
+
         if meters < 0:
-            return 'Количество метров не может быть отрицательным'
+            print('Количество метров не может быть отрицательным')
         else:
-            self.points = meters * 0.5
-            return self.points
+            points = meters * 0.5
+
+        return points
 
 class TotalPoints(PointsForPlace, PointsForMeters):
-    def __init__(self):
-        PointsForPlace.__init__(self)
-        PointsForMeters.__init__(self)
 
-    def get_total_points(self, place, meters):
-        place_points = self.get_points_for_place(place)
-        meters_points = self.get_points_for_meters(meters)
-
-        if isinstance(place_points, str):
-            return place_points
-        if isinstance(meters_points, str):
-            return meters_points
-        
-        total = place_points + meters_points
+    @staticmethod
+    def get_total_points(place, meters):
+        total = TotalPoints.get_points_for_place(place) + TotalPoints.get_points_for_meters(meters)
         return total
 
 points_for_place = PointsForPlace()
